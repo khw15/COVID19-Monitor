7d049a9428cb62a2718ed10c199233a73eafc592
The-Quiett <64533786+The-Quiett@users.noreply.github.com>
2020-07-15 15:21:07 +0700

    minor changes

--- src/App.js
+++ src/App.js
@@ -36,7 +36,7 @@ import API from './api'
 const formatter = buildFormatter(indonesiaString)
 
 const URL_TW_SHARE =
-'https://twitter.com/intent/tweet?url=https%3A%2F%2Fcovid19.i315.my.id%2F&text=Tetap%20update%20dengan%20statistik%20COVID-19%20terbaru%20dari%20jumlah%20pengetesan%20yang%20dilakukan%2C%20kasus%20positif%20dan%20aktif%2C%20angka%20kesembuhan%20dan%20kematian%20di%20Indonesia%20atau%20di%20Dunia.%20&hashtags=COVID-19%2CCOVID19%2CCORONA'
+  'https://twitter.com/intent/tweet?url=https%3A%2F%2Fcovid19.i315.my.id&text=Tetap%20update%20dengan%20statistik%20COVID-19%20terbaru%20dari%20jumlah%20pengetesan%20yang%20dilakukan%2C%20kasus%20positif%20dan%20aktif%2C%20angka%20kesembuhan%20dan%20kematian%20di%20Indonesia%20atau%20di%20Dunia.&hashtags=COVID19%2CCORONA'
 const URL_FB_SHARE =
   'https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fcovid19.i315.my.id/'
 
