cd4293f3096472358a3d9083409fd610208a4b1f
The-Quiett <64533786+The-Quiett@users.noreply.github.com>
2020-08-28 11:39:15 +0700

    Share button icon updated & and Share to WhatsApp

--- src/images/index.js
+++ src/images/index.js
@@ -12,3 +12,4 @@ export { default as BERSIN } from "./sneeze.svg"
 export { default as KUMPUL } from "./distance.svg" 
 export { default as FB } from './fb.svg'
 export { default as TW } from './tw.svg'
+export { default as WA } from './wa.svg'
