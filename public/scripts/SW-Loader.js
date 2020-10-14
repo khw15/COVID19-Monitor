b48d3fd4f8c511d39ea8e356b54b499028deb516
Zenn <afaisalalmunawar0315@gmail.com>
2020-10-14 18:21:26 +0700

    Update SW-Loader.js

--- public/scripts/SW-Loader.js
+++ public/scripts/SW-Loader.js
@@ -1,9 +1,5 @@
 if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/service-worker.js', { scope: '/' }).then(function(reg) {
-      // registration worked
-      console.log('Registration succeeded. Scope is ' + reg.scope);
-    }).catch(function(error) {
-      // registration failed
-      console.log('Registration failed with ' + error);
-    });
-};
+  window.addEventListener('load', function() {
+    navigator.serviceWorker.register('/service-worker.js');
+  });
+}
