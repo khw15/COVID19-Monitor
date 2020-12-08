6c0421b3b4c4d4fed6d9dd93c5a5ea2bee9eb3dc
Faisal Al-Munawar Fathur Rahman <64533786+Zenn315@users.noreply.github.com>
2020-12-08 23:46:23 +0700

    GA di-embedded di React, beberapa elemen di HTML dihapus dan diperbaharui. Penambahan paket "react-ga".

--- src/index.js
+++ src/index.js
@@ -2,5 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './css/index.css'
 import App from './App'
+import ReactGA from 'react-ga';
+
+ReactGA.initialize('UA-171777694-1');
+ReactGA.pageview(window.location.pathname + window.location.search);
+
+ReactGA.initialize('UA-171777694-1', {
+    debug: true,
+    titleCase: false
+});
 
 ReactDOM.render(<App />, document.getElementById('root'))
