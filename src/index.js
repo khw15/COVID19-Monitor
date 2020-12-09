b6db7b9c45d14ec3b55b882e8ad89bb6c5c9d65e
Faisal Al-Munawar Fathur Rahman <64533786+Zenn315@users.noreply.github.com>
2020-12-09 13:11:34 +0700

    implementasi service worker react, dan pembaharuan di ga

--- src/index.js
+++ src/index.js
@@ -1,15 +1,23 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import ReactGA from 'react-ga';
 import './css/index.css'
 import App from './App'
-import ReactGA from 'react-ga';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 ReactGA.initialize('UA-171777694-1');
 ReactGA.pageview(window.location.pathname + window.location.search);
 
 ReactGA.initialize('UA-171777694-1', {
     debug: true,
-    titleCase: false
+    titleCase: false,
+    standardImplementation: true,
+    gaAddress: './analytics.js'
 });
 
 ReactDOM.render(<App />, document.getElementById('root'))
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
