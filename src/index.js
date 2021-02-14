365d4e2312c168b2143e3fab41565b5473622eb8
Zenn315 <64533786+Zenn315@users.noreply.github.com>
2021-02-14 22:21:34 +0700

    Major update

--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import ReactGA from 'react-ga';
 import './css/index.css'
 import App from './App'
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 serviceWorkerRegistration.register();
+
+ReactGA.initialize('UA-171777694-1');
+ReactGA.pageview(window.location.pathname + window.location.search);
+
+ReactGA.initialize('UA-171777694-1', {
+    debug: true,
+    titleCase: false,
+    standardImplementation: true,
+});
 
 ReactDOM.render(<App />, document.getElementById('root'))
